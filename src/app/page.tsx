f32b28d90bbaa990bd30b495d3809662804b873b
IkuakiR <linyuzhao3@gmail.com>
2024-12-06 07:27:56 +0900

    create savingPage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,39 +2,50 @@
 
 import React from "react";
 import styles from '@/styles/savingPage/page.module.scss';
+import { useState } from "react";
 // import Image from 'next/image';
 
 export default function Home() {
+  const [displayValue, setDisplayValue] = useState("");
+  
+  const handleKeyPress = (value: string) => {
+    if (value === "←") {
+      setDisplayValue(displayValue.slice(0, -1));
+    } else {
+      setDisplayValue(displayValue + value);
+    }
+  };
+
   return (
     <>
-      <form action="" method="get">
-        <input type="text" name="price" />
-      </form>
-      <button>貯金する</button>
-      <table className={styles.numberKeys}>
-        <tbody>
-          <tr>
-            <td>1</td>
-            <td>2</td>
-            <td>3</td>
-          </tr>
-          <tr>
-            <td>4</td>
-            <td>5</td>
-            <td>6</td>
-          </tr>
-          <tr>
-            <td>7</td>
-            <td>8</td>
-            <td>9</td>
-          </tr>
-          <tr>
-            <td>0</td>
-            <td>00</td>
-            <td>000</td>
-          </tr>
-        </tbody>
-      </table>
+      <div className={styles.savingPageContainer}>
+        <div className={styles.numHolder}>{displayValue}</div>
+        <button className={styles.savingButton}>貯金する</button>
+        <table className={styles.numberKeys}>
+          <tbody>
+            <tr>
+              <td><button onClick={() => handleKeyPress("1")}>1</button></td>
+              <td><button onClick={() => handleKeyPress("2")}>2</button></td>
+              <td><button onClick={() => handleKeyPress("3")}>3</button></td>
+            </tr>
+            <tr>
+              <td><button onClick={() => handleKeyPress("4")}>4</button></td>
+              <td><button onClick={() => handleKeyPress("5")}>5</button></td>
+              <td><button onClick={() => handleKeyPress("6")}>6</button></td>
+            </tr>
+            <tr>
+              <td><button onClick={() => handleKeyPress("7")}>7</button></td>
+              <td><button onClick={() => handleKeyPress("8")}>8</button></td>
+              <td><button onClick={() => handleKeyPress("9")}>9</button></td>
+            </tr>
+            <tr>
+              <td><button onClick={() => handleKeyPress("00")}>00</button></td>
+              <td><button onClick={() => handleKeyPress("0")}>0</button></td>
+              <td><button onClick={() => handleKeyPress("←")}>←</button></td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </>
   );
 }
